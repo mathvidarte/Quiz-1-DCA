76c94fd1dd53f53e99933ea036205ead3ed647fd
Matheus Vidarte Sandoval <matheusvidartesandoval@MacBook-Air-de-Matheus-9.local>
2020-03-13 15:20:28 -0500

    Primer commit

---
+++ Quiz 1/src/Model/Figura.java
@@ -0,0 +1,30 @@
+package Model;
+
+import processing.core.PApplet;
+
+public class Figura {
+	int posX;
+	int posY;
+	int tam;
+	int vel;
+	int dir;
+	PApplet app;
+	
+	public Figura (int posX, int posY, PApplet app) {
+		this.posX = posX;
+		this.posY = posY;
+		this.tam = 20;
+		this.vel = 2;
+		this.dir = 1;
+		this.app = app;
+	}
+	
+
+	
+	public void mover () {
+	
+	}
+	
+	
+
+}
